ae45cc544052d81e057a1b76f634cdba33a02f5b
tdpsystem <102874566+tdpsystem@users.noreply.github.com>
2026-07-06 20:34:22 +0100

    update world_state.c to use 'World world' instead of camera and player directly

--- source/core/states/world_state.c
+++ source/core/states/world_state.c
@@ -1,95 +1,65 @@
 #include <nds.h>
-#include <stdio.h>
 
-#include "core\states\world_state.h"
-#include "core\state_machine.h"
-#include "graphics\video.h"
-#include "graphics\camera.h"
-#include "graphics\tile_renderer.h"
-#include "graphics\tilemap.h"
+#include "core/states/world_state.h"
+#include "core/state_machine.h"
+
 #include "graphics/entity_renderer.h"
-#include "input\input.h"
-#include "util\debug.h"
-#include "world\player.h"
+#include "graphics/tile_renderer.h"
+#include "graphics/video.h"
 
-static Camera gameCamera;
-static Player player;
-static TileMap worldMap;
+#include "input/input.h"
+#include "util/debug.h"
+#include "world/world.h"
 
-static void worldEnter(void) {
+static World world;
+
+static void worldStateEnter(void)
+{
     videoClearConsole();
-
-    cameraInit(&gameCamera);
-    playerInit(&player);
 
     tileRendererInit();
     entityRendererInit();
-    tileMapInit(&worldMap);
 
-    for (int y = 0; y < TILEMAP_HEIGHT; y++)
-    {
-        for (int x = 0; x < TILEMAP_WIDTH; x++)
-        {
-            if (x == 0 || y == 0 || x == TILEMAP_WIDTH - 1 || y == TILEMAP_HEIGHT - 1)
-            {
-                tileMapSet(&worldMap, x, y, 1);
-            }
-            else if ((x + y) % 2 == 0)
-            {
-                tileMapSet(&worldMap, x, y, 2);
-            }
-            else
-            {
-                tileMapSet(&worldMap, x, y, 0);
-            }
-        }
-    }
-
-    cameraSetPosition(
-        &gameCamera,
-        playerX(&player),
-        playerY(&player)
-    );
+    worldInit(&world);
 
     debugPrint("World State\n");
     debugPrint("Move with D-Pad\n");
+    debugPrint("X = Menu\n");
+    debugPrint("Y = Battle\n");
 }
 
-static void worldUpdate(void) {
-    playerUpdate(&player);
+static void worldStateUpdate(void)
+{
+    worldUpdate(&world);
 
-    cameraSetTarget(
-        &gameCamera,
-        playerX(&player),
-        playerY(&player)
-    );
-
-    cameraUpdate(&gameCamera);
-
-    if (inputIsPressed(KEY_X)) {
+    if (inputIsPressed(KEY_X))
+    {
         stateChange(GAME_STATE_MENU);
     }
 
-    if (inputIsPressed(KEY_Y)) {
+    if (inputIsPressed(KEY_Y))
+    {
         stateChange(GAME_STATE_BATTLE);
     }
 }
 
-static void worldRender(void) {
-    tileRendererRender(&worldMap, &gameCamera);
-    entityRendererRender(playerEntity(&player), &gameCamera);
+static void worldStateRender(void)
+{
+    worldRender(&world);
 }
 
-static void worldExit(void) {
+static void worldStateExit(void)
+{
 }
 
-GameState worldStateCreate(void) {
+GameState worldStateCreate(void)
+{
     GameState state;
 
-    state.enter = worldEnter;
-    state.update = worldUpdate;
-    state.render = worldRender;
-    state.exit = worldExit;
+    state.enter = worldStateEnter;
+    state.update = worldStateUpdate;
+    state.render = worldStateRender;
+    state.exit = worldStateExit;
 
     return state;
 }
